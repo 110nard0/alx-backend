031e6db9cffb8fb9a8731a84921a10a8e25e920a
psych0wh1z <ikeagudike@gmail.com>
2023-08-20 12:06:49 +0100

    Add task0 files

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    """
+    for num in range(page):
+        index = num * page_size
+
+    pages = page * page_size
+
+    return (index, pages)
