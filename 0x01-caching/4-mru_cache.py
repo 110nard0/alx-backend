b55b488567b3c89c4a75f8a9a2028e41a267dbf7
psych0wh1z <ikeagudike@gmail.com>
2023-08-24 06:16:13 +0100

    Add task4 files

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""MRUCache module"""
+
+from datetime import datetime
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache is a caching system that stores key:value pairs in a
+    public instance dictionary using the Most Recently Used algorithm
+    """
+    def __init__(self):
+        """Initializes MRUCache instance
+        """
+        super().__init__()
+        self.cache_dict = {}
+
+    def put(self, key: any, item: any):
+        """Assigns to the instance dictionary the item value for the key
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS\
+                    and key not in self.cache_data:
+                least = max(self.cache_dict.values())
+                for k, v in self.cache_dict.items():
+                    if v == least:
+                        least_recent = k
+                print(f'DISCARD: {least_recent}')
+                del self.cache_data[least_recent]
+                del self.cache_dict[least_recent]
+            self.cache_data[key] = item
+            self.cache_dict[key] = datetime.now()
+        else:
+            pass
+
+    def get(self, key: any) -> any:
+        """Returns the value in the instance dictionary linked to key
+        """
+        if key is None or not self.cache_data.get(key):
+            pass
+        else:
+            self.cache_dict[key] = datetime.now()
+            return self.cache_data.get(key)
