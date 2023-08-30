7e805df0b328d68b5faa1ee27b5297ffd87e88db
psych0wh1z <ikeagudike@gmail.com>
2023-08-30 04:15:09 +0100

    Add task5 files

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""app module"""
+
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configure app locale and timezone"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """Determine best match among app's supported languages"""
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """Emulate user login system using prefered locale and timezone"""
+    user_id = request.args.get('login_as', type=int)
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """Finds a user, if any and sets it on the Flask global object"""
+    user = get_user()
+    g.user = user
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Display a simple web homepage"""
+    return render_template('5-index.html', user=g.get('user'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
